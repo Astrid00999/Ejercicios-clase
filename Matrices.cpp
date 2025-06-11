9420d2d8522f9b8f9bfaab18b0d34ab89d4bcce9
Astrid00999 <00091525@uca.edu.sv>
2025-06-11 12:17:26 -0600

    Primer codigo funcional gracias a Dios

--- Matrices.cpp
+++ Matrices.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
   int mat[50][50];
   int i, j;
-  int m, n;
+  int m, n, acum;
 
   cout << "LLENAR UNA MATRIZ" << endl;
   cout << "Cuantas filas tendrá? " << endl;
@@ -13,20 +13,23 @@ int main() {
   cout << "Cuantas columnas tendrá? " << endl;
   cin >> m;
   cout << "Digite los datos de la matriz. " << endl;
+
   for (i = 0; i < m; i++)
     for (j = 0; j < n; j++) {
       cout << "mat[" << i << "][" << j << "]: " << endl;
       cin >> mat[i][j];
     }
 
+  acum = 0;
   for (i = 0; i < m; i++) {
     for (j = 0; j < n; j++) {
-      cout << mat[i][j] << " ";
+      //   cout << mat[i][j] << " ";
+       acum = acum + mat[i][j];
     }
-      cout << endl;
+    cout << endl;
   }
 
-
+  cout << "La suma es: " << acum << endl;
 
   return 0;
 }
