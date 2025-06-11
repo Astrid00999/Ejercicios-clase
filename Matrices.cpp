8e1995dcaad417999bdaac1281a18fb2aadf81b3
Astrid00999 <00091525@uca.edu.sv>
2025-06-11 12:24:52 -0600

    sumar por filas.

--- Matrices.cpp
+++ Matrices.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
   int mat[50][50];
+  int sumas[50];
   int i, j;
   int m, n, acum;
 
@@ -20,16 +21,20 @@ int main() {
       cin >> mat[i][j];
     }
 
-  acum = 0;
+  //   acum = 0;
   for (i = 0; i < m; i++) {
+    sumas[i] = 0;
     for (j = 0; j < n; j++) {
       //   cout << mat[i][j] << " ";
-       acum = acum + mat[i][j];
+      //    acum = acum + mat[i][j];
+
+      sumas[i] = sumas[i] + mat[i][j];
     }
     cout << endl;
   }
 
-  cout << "La suma es: " << acum << endl;
+  cout << "La suma por fila es: " << endl;
+  for (i = 0; i < m; i++) cout << sumas[i] << endl;
 
   return 0;
 }
